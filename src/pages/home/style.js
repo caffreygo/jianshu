ce152210c2ee26a6745b4c75e9746badf7203457
Jinrui Chen <caffreygo@163.com>
2020-06-15 22:39:22 +0800

    首页文章列表 首页推荐部分

--- src/pages/home/style.js
+++ src/pages/home/style.js
@@ -1,49 +1,101 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 export const HomeWrapper = styled.div`
-    overflow: hidden;
-    width: 960px;
-    margin: 0 auto;
-`
+  overflow: hidden;
+  width: 960px;
+  margin: 0 auto;
+`;
 
 export const HomeLeft = styled.div`
-    margin-left: 15px;
-    padding-top: 30px;
+  margin-left: 15px;
+  padding-top: 30px;
+  width: 625px;
+  float: left;
+  .banner-img {
     width: 625px;
-    float: left;
-    .banner-img {
-        width: 625px;
-        height: 270px;
-    }
-`
+    height: 270px;
+  }
+`;
 
 export const HomeRight = styled.div`
-    width: 240px;
-    float: right;
-`
+  width: 280px;
+  float: right;
+`;
 
 export const TopicWrapper = styled.div`
-    padding: 20px 0 10px 0;
-    overflow: hidden;
-    margin-left: -18px;
-`
+  padding: 20px 0 10px 0;
+  overflow: hidden;
+  margin-left: -18px;
+  border-bottom: 1px solid #dcdcdc;
+`;
 
 export const TopicItem = styled.div`
-    float: left;
-    background-color: #f7f7f7;
+  float: left;
+  background-color: #f7f7f7;
+  height: 32px;
+  margin-left: 18px;
+  margin-bottom: 18px;
+  padding-right: 10px;
+  line-height: 32px;
+  font-size: 14px;
+  border: 1px solid #dcdcdc;
+  border-radius: 4px;
+  .topic-pic {
+    width: 32px;
     height: 32px;
-    margin-left: 18px;
-    margin-bottom: 18px;
-    padding-right: 10px;
-    line-height: 32px;
-    font-size: 14px;
-    border: 1px solid #dcdcdc;
-    border-radius: 4px;
-    .topic-pic {
-        width: 32px;
-        height: 32px;
-        display: block;
-        float: left;
-        margin-right: 10px;
-    }
-`
+    display: block;
+    float: left;
+    margin-right: 10px;
+  }
+`;
+
+export const ListItem = styled.div`
+  padding: 20px 0;
+  overflow: hidden;
+  border-bottom: 1px solid #dcdcdc;
+  .pic {
+    display: block;
+    width: 125px;
+    height: 100px;
+    float: right;
+    border-radius: 10px;
+  }
+`;
+
+export const ListInfo = styled.div`
+  width: 500px;
+  float: left;
+  .title {
+    line-height: 27px;
+    font-size: 18px;
+    font-weight: bold;
+    color: #333;
+  }
+  .desc {
+    line-height: 24px;
+    font-size: 13px;
+    color: #999;
+  }
+`;
+
+export const RecommendWrapper = styled.div`
+  width: 280px;
+  margin: 30px 0;
+`;
+
+export const RecommendItem = styled.div`
+  width: 280px;
+  height: 50px;
+  margin-bottom: 5px;
+  background-image: url(${(props) => props.imgUrl});
+  background-size: contain;
+`;
+
+export const WriterWrapper = styled.div`
+  width: 278px;
+  border-radius: 3px;
+  border: 1px solid #dcdcdc;
+  height: 300px;
+  line-height: 300px;
+  text-align: center;
+`;
