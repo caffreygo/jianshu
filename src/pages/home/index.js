f476bc4efb655bb478ab34f7503f28dd2e45947d
Jinrui Chen <caffreygo@163.com>
2020-07-06 23:38:22 +0800

    加载更多 && 返回顶部

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,39 +1,69 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import { connect } from "react-redux";
 import List from "./components/List";
 import Recommend from "./components/Recommend";
 import Writer from "./components/Writer";
 import Topic from "./components/Topic";
-import { HomeWrapper, HomeLeft, HomeRight } from "./style";
+import { HomeWrapper, HomeLeft, HomeRight, BackTop } from "./style";
 import { actionCreators } from "./store";
 
 class Home extends Component {
-    render() {
-        return (
-            <HomeWrapper>
-                <HomeLeft>
-                    <img className="banner-img" alt="" src="https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1592200833675&di=6fc258493302450c75369d484e287e9b&imgtype=0&src=http%3A%2F%2Fattach.bbs.miui.com%2Fforum%2F201306%2F23%2F110328s72xxse7lfis9fnd.jpg" />
-                    <Topic />
-                    <List />
-                </HomeLeft>
-                <HomeRight>
-                    <Recommend />
-                    <Writer />
-                </HomeRight>
-            </HomeWrapper>
-        )
-    }
+  handleScrollTop() {
+    window.scrollTo(0, 0);
+  }
+  render() {
+    const { showScroll } = this.props;
+    return (
+      <HomeWrapper>
+        <HomeLeft>
+          <img
+            className="banner-img"
+            alt=""
+            src="https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1592200833675&di=6fc258493302450c75369d484e287e9b&imgtype=0&src=http%3A%2F%2Fattach.bbs.miui.com%2Fforum%2F201306%2F23%2F110328s72xxse7lfis9fnd.jpg"
+          />
+          <Topic />
+          <List />
+        </HomeLeft>
+        <HomeRight>
+          <Recommend />
+          <Writer />
+        </HomeRight>
+        {showScroll ? (
+          <BackTop onClick={this.handleScrollTop}>回到顶部</BackTop>
+        ) : null}
+      </HomeWrapper>
+    );
+  }
 
-    componentDidMount() {
-        this.props.changeHomeData()
-    }
+  componentDidMount() {
+    this.props.changeHomeData();
+    this.bindEvent();
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener("scroll", this.props.changeScrollTopShow);
+  }
+
+  bindEvent() {
+    window.addEventListener("scroll", this.props.changeScrollTopShow);
+  }
 }
 
-const mapDispatch = (dispatch)=> ({
-    changeHomeData() {
-        const action = actionCreators.getHomeInfo();
-        dispatch(action)
-    }
-})
+const mapState = (state) => ({
+  showScroll: state.getIn(["home", "showScroll"]),
+});
 
-export default connect(null,mapDispatch)(Home);
+const mapDispatch = (dispatch) => ({
+  changeHomeData() {
+    dispatch(actionCreators.getHomeInfo());
+  },
+  changeScrollTopShow(e) {
+    if (document.documentElement.scrollTop > 400) {
+      dispatch(actionCreators.toggleTopShow(true));
+    } else {
+      dispatch(actionCreators.toggleTopShow(false));
+    }
+  },
+});
+
+export default connect(mapState, mapDispatch)(Home);
--- src/pages/home/store/actionCreators.js
+++ src/pages/home/store/actionCreators.js
@@ -1,17 +1,39 @@
 import axios from "axios";
+import { fromJS } from "immutable";
 import * as constants from "./constants";
 
-const changeHomeData = (result)=> ({
+const changeHomeData = (result) => ({
   type: constants.CHANGE_HOME_DATA,
   topicList: result.topicList,
   articleList: result.articleList,
-  recommendList: result.recommendList
-})
+  recommendList: result.recommendList,
+});
 
-export const getHomeInfo = ()=> {
-  return (dispatch)=>
-  axios.get("/api/home.json").then(res=>{
-    const {success,data} = res.data;
-    success && dispatch(changeHomeData(data));
-})
-}
+const addHomeList = (list, nextPage) => ({
+  type: constants.ADD_ARTICLE_LIST,
+  list: fromJS(list),
+  nextPage,
+});
+
+export const getHomeInfo = () => {
+  return (dispatch) => {
+    axios.get("/api/home.json").then((res) => {
+      const { success, data } = res.data;
+      success && dispatch(changeHomeData(data));
+    });
+  };
+};
+
+export const getMoreList = (page) => {
+  return (dispatch) => {
+    axios.get(`/api/homeList.json?page=${page}`).then((res) => {
+      const data = res.data.data;
+      dispatch(addHomeList(data, page + 1));
+    });
+  };
+};
+
+export const toggleTopShow = (show) => ({
+  type: constants.TOGGLE_SCROLL_TOP,
+  show,
+});
